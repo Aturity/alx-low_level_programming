40989b60a58eedb9d483cb7dbc5b2ea99ef8dff7
Aturity <atuhgab@gmail.com>
2023-02-17 17:27:14 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
  * main - prints "with proper grammar, but the outcome is a piece of art,"
  * Return: always 0 (success).
  */
-
 int main(void)
 
 {
-	printf(with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
