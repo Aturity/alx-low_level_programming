43309a012aad6ca94651cb2ec410b61b1867fbd0
Aturity <atuhgab@gmail.com>
2023-02-17 20:01:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error
+ * followed by a newline, to standard error.
  * Return: Always 1.
  */
 
-int main(void)
+int main(void)o
 
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
