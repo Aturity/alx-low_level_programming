9f2f32f56e818bb93af0e483e228d60e3605dda9
Aturity <atuhgab@gmail.com>
2023-05-12 05:27:18 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,54 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints to the standard output
- * @filename: this the file being read
- * @letters: numbers of letters printed.
- *
- * Return: the actual number of letters it could read and print
- * if It fails, returns 0.
+ * read_textfile-a function that reads a text file
+ * and prints it to the POSIX STDOUT.
+ * @filename: filename
+ * @letters: the number of letters to be read
+ * Return: the actual number of letters read and printed
+ * or 0 if it fails or filename is NULL.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int fd;
+	ssize_t _read, _write;
+
+	char *buffer
+
+	if (!filename)
+
+	{
+	return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	return (0);
 
-	free(buf);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	return (0);
+
+	_read = read(fd, buffer, letters);
+	if (_READ == -1)
+
+	{
+	free(buffer);
 	close(fd);
-	return (w);
+	return (0);
+	}
+
+	_write write(STDOUT_FILE, buffer, read);
+	if (_write == -1)
+
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+		close(fd);
+		return (_read);
 }
