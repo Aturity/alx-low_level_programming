e382302edcec7d506eb7b60be5e584e09888d975
Aturity <atuhgab@gmail.com>
2023-05-12 06:38:52 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,45 +11,30 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
 	int fd;
-	ssize_t _read, _write;
-
-	char *buffer
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
-
-	{
-	return (0);
-	}
+		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
-	return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	return (0);
-
-	_read = read(fd, buffer, letters);
-	if (_READ == -1)
-
-	{
-	free(buffer);
-	close(fd);
-	return (0);
-	}
-
-	_write write(STDOUT_FILE, buffer, read);
-	if (_write == -1)
-
-	{
-		free(buffer);
-		close(fd);
 		return (0);
-	}
 
-		close(fd);
-		return (_read);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
+
